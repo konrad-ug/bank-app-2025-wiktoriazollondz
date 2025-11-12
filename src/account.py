85a3b1a5b75a9d1273a7797f3569b330d762ce5b
zoriaa <w.zollondz.037@studms.ug.edu.pl>
2025-11-12 16:11:01 +0100

    feature 11

--- src/account.py
+++ src/account.py
@@ -1,16 +1,21 @@
 class Account:
-    def __init__(self, balance=0):
+    def __init__(self, balance=0, history=None):
         self.balance = balance
+        self.history = [] if history is None else history
 
     def incoming_transfer(self, amount: float) -> None:
         if amount > 0:
             self.balance += amount
+            self.history.append(amount)
     
     def outgoing_transfer(self, amount: float) -> None:
         if amount > 0 and amount <= self.balance:
             self.balance -= amount
+            self.history.append(-amount)
 
     def express_transfer(self, amount: float) -> None:
         fee = 1.0
         if amount > 0 and (self.balance + fee) >= amount:
             self.balance -= (amount + fee)
+            self.history.append(-amount)
+            self.history.append(-fee)
